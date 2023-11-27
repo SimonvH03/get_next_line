a994ae3287ec1b6efd358d5989e4cf41760a9850
simon <si.hoogstraaten86@gmail.com>
2023-11-27 18:00:46 +0100

    writing

--- rewrite/testmain/testmain.c
+++ rewrite/testmain/testmain.c
@@ -3,43 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   testmain.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 21:46:08 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/11/24 22:10:40 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/11/25 00:08:50 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	else
+		return (0);
+}
 
 void	ft_writing(char *str, int n)
 {
-	if (n <= 0)
-	{
-		while (*str)
-		{
-			write(1, str++, 1);
-		}
-	}
-	else
-	{
-		while (n--)
-		{
-			if (*str == '\0')
-				write(1, "\\0", 2);
-			else if (*str == '\n')
-				write(1, "\\n", 2);
-			else
-				write(1, str++, 1);
-		}
-	}
+	char esc[7];
 
+	esc[0] = '[';
+	esc[1] = '\\';
+	esc[6] = ']';
+	while (*str || n > 0)
+	{
+		if (!ft_isprint(*str))
+		{
+			esc[1] = ((*str >> 6) & 7) + '0';
+			esc[2] = ((*str >> 3) & 7) + '0';
+			esc[3] = ((*str >> 0) & 7) + '0';
+			write(1, esc, 7);
+			str++;
+		}
+		else
+			write(1, str++, 1);
+		if (n > 0)
+			n--;
+	}
+	if (n < 0)
+		write(1, "\\0", 2);
 }
 
 int	main(void)
 {
-	char *str = "randoms\nhit";
+	char *str = "line00\nline01";
 	static char	buffer[10];
-	ft_writing(str, 15);
+	ft_writing(str, 0);
+	write(1, "\n", 1);
 	return (0);
 }
